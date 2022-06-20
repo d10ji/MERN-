fabc1361955d2ed0c6504379d54d990018927b3f
Datenji SHerpa <sherpa.datenji@gmail.com>
2022-06-20 18:36:19 +0545

    this is 6/20

--- node js/npm-basic/index.js
+++ node js/npm-basic/index.js
@@ -14,5 +14,17 @@
 
 console.log(validator.isEmail("sherpa.datenji@gmail.com"));
 
+console.log(chalk.green(
+	'I am a green line ' +
+	chalk.blue.underline.bold('with a blue substring') +
+	' that becomes green again!'
+));
 
+
+// ES2015 template literal
+console.log(`
+CPU: ${chalk.red('90%')}
+RAM: ${chalk.green('40%')}
+DISK: ${chalk.yellow('70%')}
+`);
 
